350d1585a7a6e00eebdd8224f62abaf90132d1a9
Kotaba <prosperodesu@live.com>
2016-01-25 12:50:26 +0000

    Change school select max length

--- SpeakUpIn/forms.py
+++ SpeakUpIn/forms.py
@@ -23,4 +23,4 @@ class NewTicketForm(forms.Form):
     middle_name = forms.CharField(max_length=50, label="Отчество", required=False)
     city = forms.CharField(max_length=10, label="Город", required=True)
     age = forms.CharField(max_length=50, label="Возраст", required=True)
-    school = forms.CharField(max_length=50, label="Школа", required=True)
+    school = forms.CharField(max_length=200, label="Школа", required=True)
